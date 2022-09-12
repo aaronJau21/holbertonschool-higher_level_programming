ea78494298085c655985e83b5a53b953a9d206b2
Aaron <aaronjau21@gmail.com>
2022-09-11 22:20:54 -0500

    3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-from sys import argv
-
-if (__name__ == "__main__"):
-    i = 1
+if __name__ == "__main__":
+    import sys
     result = 0
-    arguments = len(argv) - 1
-
-    if (arguments != 0):
-        while (i <= arguments):
-            result += int(argv[i])
-            i += 1
-    print(result)
+    for i in range(1, len(sys.argv)):
+        result += int(sys.argv[i])
+    print("{}".format(result))
