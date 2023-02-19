d7630b761f463f3d7b0508d9bad2165d6f430f5d
Aaron <aaronjau21@gmail.com>
2023-02-18 21:36:41 -0500

    cambios

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,4 +1,82 @@
-@classmethod
+#!/usr/bin/python3
+""" Module that contains class Base """
+
+from os import path
+from fileinput import filename
+import json
+
+
+class Base:
+    """ Class Base """
+    __nbobjects = 0
+
+    def __init__(self, id=None):
+        """ Initializes instances """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nbobjects += 1
+
+            self.id = Base.__nbobjects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Convert list to JSON string"""
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ Save object in a file """
+        filename = "{}.json".format(cls.__name__)
+        list_dic = []
+
+        if list_objs is None:
+            print("OK", end="")
+        for i in range(len(list_objs)):
+            list_dic.append(list_objs[i].to_dictionary())
+
+        lists = cls.to_json_string(list_dic)
+
+        with open(filename, 'w') as f:
+            f.write(lists)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ JSON string to dictionary """
+        if not json_string:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        if cls.__name__ == 'Square':
+            dummy = cls(3)
+
+        if cls.__name__ == 'Rectangle':
+            dummy = cls(3, 3)
+
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        filename = cls.__name__ + '.json'
+
+        if path.exists(filename) is False:
+            return []
+
+        with open(filename, mode='r', encoding='utf-8') as f:
+            objs = cls.from_json_string(f.read())
+            instances = []
+
+            for elem in objs:
+                instances.append(cls.create(**elem))
+
+            return instances
+
+    @classmethod
     def create(cls, **dictionary):
         """
         Method that return an object with all fields already set
