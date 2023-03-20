479e3f17619d4410f4cb9a844144a18eb1eae52d
aaronJau21 <aaronjau21@gmail.com>
2023-03-19 22:50:07 -0500

    0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
-''' Script to retrieve data from a database '''
-import sys
-import MySQLdb
+"""lists all states from the database hbtn_0e_0_usa"""
 
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb as mysql
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],
-                         host='localhost', port=3306)
+    try:
+        db = mysql.connect(host='localhost', port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3])
+    except Exception:
+        print('Can not connect to the database')
+        exit(0)
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id")
-    [print(state) for state in cur.fetchall()]
-    cur.close()
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC;")
+
+    result_query = cursor.fetchall()
+
+    for row in result_query:
+        print(row)
+
+    cursor.close()
     db.close()
