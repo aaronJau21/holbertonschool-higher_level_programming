7e2fc96cd2c64db3d0bccb060c72df8b757cba5d
aaronJau21 <aaronjau21@gmail.com>
2023-04-23 23:32:45 -0500

    jqury

---
+++ javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+const redHeader = $('DIV#red_header');
+const header = $('header');
+$(redHeader).click(function () {
+    header.css('color', '#FF0000');
+});
---
+++ javascript-web_jquery/4-script.js
@@ -0,0 +1,5 @@
+const redHeader = $('DIV#toggle_header');
+const header = $('header');
+$(redHeader).click(function () {
+    header.toggleClass('red green');
+});
