9f4580bb6ec7733edad72dd4a9008db661b4c8c6
Aaron Jauregui <aaronjau21@gmail.com>
2023-05-21 10:11:37 -0500

    2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
 
-const { argv } = require('process');
-const argc = argv.length;
-
-if (argc === 2) {
+if (!process.argv[2]) {
     console.log('No argument');
-} else if (argc === 3) {
+} else if (process.argv[2] && !process.argv[3]) {
     console.log('Argument found');
 } else {
     console.log('Arguments found');
