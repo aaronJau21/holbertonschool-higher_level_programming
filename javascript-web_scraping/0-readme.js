4734f64b11260d244e65e0b8c359ebdfd2e414ca
Aaron Jauregui <aaronjau21@gmail.com>
2023-05-21 10:16:13 -0500

    0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-    console.log(error || content);
+
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) throw err;
+  console.log(data);
 });
