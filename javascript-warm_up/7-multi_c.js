979e9836ce0f8473e760d757056769957b9f5d69
AaronJau21 <aaronjau@gmail.com>
2023-05-28 07:11:47 -0700

    7-multi_c.js

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-
-const { argv } = require('process');
-const num = parseInt(argv[2]);
-const printC = (cuantity) => {
-    for (; cuantity > 0; cuantity--) console.log('C is fun');
-};
-
-Number.isInteger(num) ? printC(num) : console.log('Missing number of occurrences');
+const [, , arg] = process.argv;
+const x = parseInt(arg);
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
