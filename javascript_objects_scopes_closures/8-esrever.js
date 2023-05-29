2bdc41b981f56534ebbe70b83acbd8ea92b2188e
AaronJau21 <aaronjau@gmail.com>
2023-05-28 17:36:19 -0700

    8-esrever.js

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-
-const esrever = (list) => {
-    const reversedArray = [];
-    list.forEach((element) => reversedArray.unshift(element));
-    return (reversedArray);
+exports.esrever = function (list) {
+  const newList = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    newList.push(list[i]);
+  }
+  return newList;
 };
-
-module.exports = { esrever };
