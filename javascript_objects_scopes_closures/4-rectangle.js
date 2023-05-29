6783476459ddb0b6e280f35a494190062a4519c9
AaronJau21 <aaronjau@gmail.com>
2023-05-28 17:34:21 -0700

    4-rectangle.js

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,29 +1,28 @@
 #!/usr/bin/node
-
-class Rectangle {
-    constructor(w, h) {
-        if (w > 0 && h > 0) {
-            this.width = w;
-            this.height = h;
-        }
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
+  }
 
-    print() {
-        for (let i = 0; i < this.height; i++) {
-            console.log('X'.repeat(this.width));
-        }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) { process.stdout.write('X'); }
+      console.log('');
     }
+  }
 
-    rotate() {
-        const tmp = this.height;
-        this.height = this.width;
-        this.width = tmp;
-    }
+  rotate () {
+    const a = this.width;
+    const b = this.height;
+    this.width = b;
+    this.height = a;
+  }
 
-    double() {
-        this.height *= 2;
-        this.width *= 2;
-    }
-}
-
-module.exports = Rectangle;
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
