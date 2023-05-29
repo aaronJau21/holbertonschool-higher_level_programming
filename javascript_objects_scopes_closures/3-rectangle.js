f1cc5e788f84b8f3ad5ccc3bbd445b3b9fc4c4b5
AaronJau21 <aaronjau@gmail.com>
2023-05-28 17:33:52 -0700

    3-rectangle.js

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,18 +1,16 @@
 #!/usr/bin/node
-
-class Rectangle {
-    constructor(w, h) {
-        if (w > 0 && h > 0) {
-            this.width = w;
-            this.height = h;
-        }
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
+  }
 
-    print() {
-        for (let i = 0; i < this.height; i++) {
-            console.log('X'.repeat(this.width));
-        }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) { process.stdout.write('X'); }
+      console.log('');
     }
-}
-
-module.exports = Rectangle;
+  }
+};
