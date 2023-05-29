b2ccf03991b6c06b5063b91403ecb90eb9eaffb0
AaronJau21 <aaronjau@gmail.com>
2023-05-28 18:04:40 -0700

    3-starwars_title.js

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,27 @@
 #!/usr/bin/node
+
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-    console.log(error || JSON.parse(body).title);
+const args = process.argv.slice(2);
+
+if (args.length < 1) {
+  console.log('Por favor proporcione el ID de la película como argumento');
+  process.exit(1);
+}
+
+const movieId = args[0];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(url, function(error, response, body) {
+  if (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
+
+  if (response.statusCode !== 200) {
+    console.error(`Error: API devolvió el código de estado ${response.statusCode}`);
+    process.exit(1);
+  }
+
+  const movieData = JSON.parse(body);
+  console.log(`${movieData.title}`);
 });
