9c42dcc6a39e290fe04fe9e7ad79f8bbb7078a41
AaronJau21 <aaronjau@gmail.com>
2023-05-28 17:57:40 -0700

    0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf-8', function (err, data) {
-  if (err) throw err;
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
   console.log(data);
 });
