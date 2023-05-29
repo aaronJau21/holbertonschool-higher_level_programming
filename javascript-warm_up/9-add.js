1b0ffe5fb93f454ef84991e7d2642b9e4eb82df2
AaronJau21 <aaronjau@gmail.com>
2023-05-28 17:26:08 -0700

    9-add.js

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
+const argv = process.argv;
+const x = parseInt(argv[2]);
+const y = parseInt(argv[3]);
 
-function add(a, b) {
-    return (a + b);
+function add (a, b) {
+  return (a + b);
 }
 
-const { argv } = require('process');
-const num1 = parseInt(argv[2]);
-const num2 = parseInt(argv[3]);
-
-console.log(add(num1, num2));
+console.log(add(x, y));
