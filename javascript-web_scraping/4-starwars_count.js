ad63c474d0882a838ac4b9f1d80941e2732d1161
AaronJau21 <aaronjau@gmail.com>
2023-05-29 19:10:33 -0700

    4-starwars_count.js

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,18 +1,21 @@
 #!/usr/bin/node
-const request = require('request');
 const url = process.argv[2];
-let count = 0;
-request.get(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
+const request = require('request');
+const id = '18';
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
   } else {
-    const results = JSON.parse(body).results;
-    for (let i = 0; i < results.length; i++) {
-      const cha = results[i].characters;
-      for (let j = 0; j < cha.length; j++) {
-        if (cha[j].slice(cha[j].length - 3, cha[j].length - 1) === '18') { count += 1; }
+    const jsonBody = JSON.parse(body);
+    let counter = 0;
+    for (const res of jsonBody.results) {
+      for (const char of res.characters) {
+        if (char.search(id) > 0) {
+          counter++;
+        }
       }
     }
-    console.log(count);
+    console.log(counter);
   }
 });
