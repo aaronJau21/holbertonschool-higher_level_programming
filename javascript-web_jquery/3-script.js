7d0519c9d2b0564f03b87732a27791dd45df568a
AaronJau21 <aaronjau@gmail.com>
2023-05-29 19:18:53 -0700

    3-script.js

--- javascript-web_jquery/3-script.js
+++ javascript-web_jquery/3-script.js
@@ -1,5 +1,5 @@
-const redHeader = $('DIV#red_header');
-const header = $('header');
-$(redHeader).click(function () {
-    header.addClass('red');
+$(function () {
+  $('#red_header').click(function () {
+    $('header').addClass('red');
+  });
 });
